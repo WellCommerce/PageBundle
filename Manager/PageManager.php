acc15352b20a2b3c35b7694ac91061db2270bbae
Adam Piotrowski <adam@wellcommerce.org>
2016-04-29 13:53:16 +0200

    Changed extension configurations

--- Manager/PageManager.php
+++ Manager/PageManager.php
@@ -10,15 +10,15 @@
  * please view the LICENSE file that was distributed with this source code.
  */
 
-namespace WellCommerce\Bundle\PageBundle\Manager\Front;
+namespace WellCommerce\Bundle\PageBundle\Manager;
 
-use WellCommerce\Bundle\CoreBundle\Manager\Front\AbstractFrontManager;
+use WellCommerce\Bundle\CoreBundle\Manager\AbstractManager;
 
 /**
  * Class PageManager
  *
  * @author  Adam Piotrowski <adam@wellcommerce.org>
  */
-class PageManager extends AbstractFrontManager
+class PageManager extends AbstractManager
 {
 }
